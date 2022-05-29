16b5620db4f1816729627d5a6ac863ebbcd98a35
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2022-05-29 15:39:01 +0100

    add next conifg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withLinaria = require('next-linaria');
+const nextConfig = withLinaria({
+	reactStrictMode: true,
+	images: { domains: ['i.picsum.photos'] },
+});
+
+module.exports = nextConfig;
