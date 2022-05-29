98e33dae3ae7f0edac2f9e606277a8850564d7dd
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2022-05-29 15:39:50 +0100

    start theme

---
+++ styles/theme.ts
@@ -0,0 +1,37 @@
+export const theme = {
+	colors: {
+		black: '#000B0E',
+		white: '#FFFCFC',
+		gray: {
+			light: '#A2A7A8',
+			dark: '#666D6E',
+		},
+	},
+	font: {
+		family: {
+			title: 'Lato, sans-serif',
+			display: "'Crimson Text', sans-serif",
+			heading: "'Bona Nova', sans-serif",
+			paragraph: 'Lato, sans-serif',
+		},
+		size: {
+			xs: '0.5rem',
+			sm: '0.875rem',
+			md: '1rem',
+			lg: '1.25rem',
+			xl: '1.5rem',
+			xxl: '2rem',
+		},
+	},
+	breakPoint: {
+		xxs: '320px',
+		xs: '375px',
+		sm: '425px',
+		md: '768px',
+		lg: '1024px',
+		xl: '1440px',
+		xxl: '2560px',
+	},
+} as const;
+
+export default theme;
