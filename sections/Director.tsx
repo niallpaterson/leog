f883b0e74108ee6968e4b0b005aa8f1b921b2290
NiallJP <niall@biglemon.co.uk>
2022-08-28 10:35:57 +0100

    start director section

--- sections/Director.tsx
+++ sections/Director.tsx
@@ -2,15 +2,7 @@ import * as React from 'react';
 import { css } from 'linaria';
 import { Grid, Heading } from '../components';
 import Image from 'next/image';
-import {
-	Title,
-	Caption,
-	Display,
-	Paragraph,
-	ImageClip,
-	IntersectionAnchor,
-	ExternalLink,
-} from '../components';
+import { Paragraph, ImageClip, IntersectionAnchor } from '../components';
 import Director1 from '../public/images/director-1.jpg';
 import theme from '../styles/theme';
 
@@ -19,41 +11,33 @@ export const Director: React.FC = () => {
 
 	return (
 		<Grid.Item as='section' start={1} end={13} subGrid>
-			<Grid.Item start={2} end={13}>
+			<Grid.Item start={3} end={12}>
 				<Heading>AS A DIRECTOR, I AM LED BY THE ACTOR AND TEXT.</Heading>
 			</Grid.Item>
 			<Grid.Item subGrid start={1} end={12}>
-				<Grid.Item
-					start={2}
-					end={11}
-					className={css`
-						/* position: relative;  */
-					`}
-				>
+				<Grid.Item start={3} end={12} paddingTop={theme.spacing.xs}>
 					<ImageClip height='120px'>
 						<Image
 							src={Director1}
 							alt=''
 							layout='responsive'
 							width='100%'
-							height='80px'
+							height={theme.spacing.md}
 						/>
 					</ImageClip>
 				</Grid.Item>
-				<Grid.Item start={2} end={8} paddingTop={theme.spacing.sm}>
+				<Grid.Item start={3} end={7} paddingTop={theme.spacing.sm}>
 					<Paragraph>
-						<span>As director, assistant director, and acting coach,</span>
-						<br />
-						<span> For theatre and opera alike,</span>
-						<br />I let the union of performer and text shape my craft.
+						I let the union of performer and text shape my craft.
 					</Paragraph>
 				</Grid.Item>
-				<Grid.Item start={7} end={12} paddingTop={theme.spacing.md}>
+				<Grid.Item start={7} end={11} paddingTop={theme.spacing.lg}>
 					<Paragraph>
-						I lead my theatre company{' '}
+						I lead my theatre company
 						<span style={{ textDecoration: 'underline' }}>
-							The Albion Theatre
-						</span>{' '}
+							{' '}
+							The Albion Theatre{' '}
+						</span>
 						on nationwide tours of theatres, gardens, and schools.
 					</Paragraph>
 				</Grid.Item>
