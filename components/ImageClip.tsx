e97b2636524247e018498ba7386e8e4093bc15f3
NiallJP <niall@biglemon.co.uk>
2022-08-28 10:35:11 +0100

    update image clip

--- components/ImageClip.tsx
+++ components/ImageClip.tsx
@@ -4,10 +4,31 @@ import { styled } from 'linaria/react';
 type ImageClipProps = {
 	height?: string;
 	width?: string;
+	children?: React.ReactNode;
 };
 
-export const ImageClip = styled.div<ImageClipProps>`
+export const ImageClip: React.FC<ImageClipProps> = ({
+	height,
+	width,
+	children,
+}) => {
+	return (
+		<Clip height={height} width={width}>
+			<Container>{children}</Container>
+		</Clip>
+	);
+};
+
+const Clip = styled.div<ImageClipProps>`
 	overflow: clip;
+	flex: 1;
+
 	height: ${({ height }) => height ?? '100%'};
 	width: ${({ width }) => width ?? '100%'};
+	display: flex;
+	align-items: center;
+`;
+
+const Container = styled.div<ImageClipProps>`
+	flex: 1;
 `;
